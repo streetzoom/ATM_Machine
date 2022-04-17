cdc9c82a9122145d6bdb192c9d72ee704f7d092b
streetzoom <brilillja81@gmail.com>
2022-04-17 17:27:18 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+/**TODO:
+ * 1:main_menu
+ * 2:get_input
+ * 3:login function
+ * 4:register_user
+ * get_username
+ * get_password
+ * save_user
+ * 5:fail attempts
+ * 6:
+**/
+
+
+int main()
+{
+
+    std::cin.get();
+    return 0;
+}
+
