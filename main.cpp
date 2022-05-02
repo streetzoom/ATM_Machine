e41ef523196e64f5f6ff6587df67cc04da70d270
Ilya Bril <brilillja81@gmail.com>
2022-05-02 15:57:17 +0300

    new functionality added
    adde showBalance, withdraw func. main loop and switch

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <string_view>
 #include <cstdlib>
 /**TODO:
@@ -19,12 +20,11 @@
 	withdrawl(): This function helps to withdraw money from the account.
 	main(): This function there is a simple switch case (to make choices) inside an infinite while loop so that every time user gets to select choices.
 **/
-
 class Bank {
 private:
 
-	std::string_view name;
-	int account_number;
+	std::string name;
+	int account_number = 0;
 	int type{ 10 };
 	int amount = 0;
 	int total = 0;
@@ -34,45 +34,93 @@ public:
 	void setValue()
 	{
 		std::cout << "Enter name\n";
-		std::getline(std::cin, &name);
+		std::cin.ignore();
+
+		std::getline(std::cin, name);
 
 		std::cout << "Enter Account number\n";
-		std::cin << &account_number;
+		std::cin >> account_number;
 
 		std::cout << "Enter Account type\n";
-		std::cin << &type;
+		std::cin >> type;
 
 		std::cout << "Enter Balance\n";
-		std::cin << &total;
+		std::cin >> total;
 	}
 
 	void showData()
 	{
 		std::cout << "Account Name: " << &name << std::endl;
-		std::cout << "Account ¹: " << &account_number << std::endl;
-		std::cout << "Account type: " << &type << std::endl;
-		std::cout << "Enter Account number\n";
-		std::cout << "Balance: " << &total << std::endl;
+		std::cout << "Account No: " << account_number << std::endl;
+		std::cout << "Account type: " << type << std::endl;
+		std::cout << "Balance: " << total << std::endl;
 	}
 
 	void deposit()
 	{
 		std::cout << "Enter amount to be deposited\n";
-		std::cin >> &amount;
+		std::cin >> amount;
+	}
+
+	void showBalance()
+	{
+		total += amount;
+		std::cout << "\nTotal balance is: \n" << total;
+	}
+
+	void withdraw()
+	{
+		int wd, available_balance;
+		std::cout << "Enter amount to withdraw\n";
+		std::cin >> wd;
+		available_balance = total - wd;
+		std::cout << "Available balance is" << available_balance;
 	}
 };
     
-    int main()
-    {			
-		Bank b;
-		
-        
-        std::cout << "HELLO WORLD!" << std::endl;
-        std::cin.get(); // alternative to std::system("pause");
-        return 0;
-    }
+int main()
+{
+	Bank b;
+	int choice;
 
+	while (1) {
+		std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			<< "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			<< "~~~WELCOME~~~~~~~~~~~~~~~~~~"
+			<< "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			<< "~~~~~~~~~\n\n";
+		std::cout << "Enter Your Choice\n";
+		std::cout << "\t1. Enter name, Account "
+			<< "number, Account type\n";
+		std::cout << "\t2. Balance Enquiry\n";
+		std::cout << "\t3. Deposit Money\n";
+		std::cout << "\t4. Show Total balance\n";
+		std::cout << "\t5. Withdraw Money\n";
+		std::cout << "\t6. Cancel\n";
+		std::cin >> choice;
 
-
-
-
+		switch (choice)
+		{
+		case 1:
+			b.setValue();
+			break;
+		case 2:
+			b.showData();
+			break;
+		case 3:
+			b.deposit();
+			break;
+		case 4:
+			b.showBalance();
+			break;
+		case 5:
+			b.withdraw();
+			break;
+		case 6:
+			exit(1);
+			break;
+		default:
+			std::cout << "\nInvalid input\n";
+		}
+	}
+}
